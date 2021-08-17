6c2c0f7d0492f65aa721cecfc9a10d3d9ccdbbd2
Yulia <yulia.v@Yulias-MacBook.local>
2021-08-17 17:01:00 +0700

    Updated text

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //import * as cowsay from "cowsay";
 
 const cowsay = require("cowsay");
-console.log(cowsay.say({ text: " I am a moooodule" }));
+//console.log(cowsay.say({ text: " I am a moooodule" }));
+console.log(cowsay.say({ text: "Hello, world" }));
